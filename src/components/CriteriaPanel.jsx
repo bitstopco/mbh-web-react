c8d18c8677b799a6fceed6047fdae3a3237f654c
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-11-20 20:29:39 -0500

    CriteridaPanel

--- src/components/CriteriaPanel.jsx
+++ src/components/CriteriaPanel.jsx
@@ -6,38 +6,38 @@ import Base from './Base';
 export default class CriteriaPanel extends Base{
     render(){
         return ( 
-            <div className='CriteriaPanel'>
-            	<div className='root_container column'>
-            		<p className=''>Criteria</p>
+            <div className='CriteriaPanel '>
+            	<div className='root_container column '>
+            		<p className='header'>Criteria</p>
 
-	                <div className=' '>
+	                <div className='text_container row'>
 
-	                	<div  className=' '>
-	                		<div className=' '>	
+	                	<div  className='box column'>
+	                		<div className='title column'>	
 			             		<p>Creativity</p>
-			             		<span className=''/>
+			             		<span className='bar'/>
 			                </div>
 			                <p className=''>Another bitcoin wallet may not add much wow factor for your team unless it has very uniqure features. How could bitcoin enchance pain points in gaming or paywalls</p>
 	                	</div>
 	                	
-	                	<div className=' '>
-	                		<div className=' '>	
+	                	<div className='box column'>
+	                		<div className='title column'>	
 			             		<p>Product</p>
-			             		<span className=''/>
+			             		<span className='bar'/>
 			                </div>
 			                <p className=''>Does the application make bitcoin more user friendly and approachable? Can the application make bitcoin go viral or much easier to use?</p>
 	                	</div>
 	                	
-	                	<div className=' '>
-	                		<div className=' '>	
+	                	<div className='box column'>
+	                		<div className='title column'>	
 			             		<p>Execution</p>
-			             		<span className=''/>
+			             		<span className='bar'/>
 			                </div>
 			                <p className=''>The devil is in the details. Does the application work well? Does it provide a good experience to the user? How does UX feel or the GUI look?</p>
 	                	</div>
 	                </div>
 
-	                <p className=''>Judges</p>
+	                <p className='header'>Judges</p>
             	</div>
             </div>
         );
