adeebbd41f4c3e566d3bb2537085d13d0642adf4
Oscar Lafarga <otech47@gmail.com>
2016-11-28 05:54:58 -0500

    observer registration bugfix

--- src/components/RegisterOverlay.jsx
+++ src/components/RegisterOverlay.jsx
@@ -13,7 +13,8 @@ export default class RegisterOverlay extends Base{
         this.state = {
             name: '',
             email: '',
-            error: ''
+            error: '',
+            success: false
         };
     }
     handleRegistration() {
@@ -22,11 +23,12 @@ export default class RegisterOverlay extends Base{
         postData.append('observer[email]', this.state.email);
         postData.append('observer[name]', this.state.name);
 
-        api.postExt('http://register.miamibitcoinhackathon.com/observers', postData)
+        // api.postExt('http://register.miamibitcoinhackathon.com/observers', postData)
+        api.postExt('http://miamibitcoinhackathon.com:3000/observers', postData)
         .then(data => {
             console.log('postdaata')
             console.log(data);
-            this.props.dismissOverlay();
+            this.setState({ success: true });
         })
         .catch(err => {
             console.log('err')
@@ -42,30 +44,44 @@ export default class RegisterOverlay extends Base{
 
         return ( 
             <div className={'RegisterOverlay column' + (this.props.hidden ? ' hide' : '')}>
-                <div className='window column'>
-                    <h6>Observer Registration</h6>
-                    <div className='field-container'>
-                        <div className='field row'>
-                            <p>Name</p>
-                            <input type='text' valueLink={link('name')}/>
-                        </div>
-                        <div className='field row'>
-                            <p>Email</p>
-                            <input type='text' valueLink={link('email')}/>
-                        </div>
-                    </div>
-                    <div className='error'>{this.state.error}</div>
-                    <div className='button-container row'>
+                {this.state.success ?
+                    <div className='window column success'>
+                        <p>Email registered successfully</p>
                         <Button
-                            style={{ backgroundColor: '#eeeeee' }}
+                            rounded
+                            style={{ backgroundColor: '#ff0098' }}
                             onClick={this.props.dismissOverlay}
                         >
-                            <span style={{color: '#313542'}}>Cancel</span>
+                            <span style={{color: '#ffffff'}}>Okay</span>
                         </Button>
-                        <span/>
-                        <Button onClick={this.handleRegistration}>Register</Button>
                     </div>
-                </div>
+                    :
+                    <div className='window column'>
+                        <h6>Observer Registration</h6>
+                        <div className='field-container'>
+                            <div className='field row'>
+                                <p>Name</p>
+                                <input type='text' valueLink={link('name')}/>
+                            </div>
+                            <div className='field row'>
+                                <p>Email</p>
+                                <input type='text' valueLink={link('email')}/>
+                            </div>
+                        </div>
+                        <div className='error'>{this.state.error}</div>
+                        <div className='button-container row'>
+                            <Button
+                                style={{ backgroundColor: '#eeeeee' }}
+                                onClick={this.props.dismissOverlay}
+                            >
+                                <span style={{color: '#313542'}}>Cancel</span>
+                            </Button>
+                            <span/>
+                            <Button onClick={this.handleRegistration}>Register</Button>
+                        </div>
+                    </div>
+                }
+                
             </div>
         );
     }
