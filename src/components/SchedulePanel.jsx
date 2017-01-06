2454405325390eb2ee066970186144ccb5428d55
Oscar Lafarga <otech47@gmail.com>
2017-01-06 13:04:41 -0500

    schedule change

--- src/components/SchedulePanel.jsx
+++ src/components/SchedulePanel.jsx
@@ -12,15 +12,27 @@ export default class SchedulePanel extends Base{
                     <div className='bar'/>
                     <span>26 hours of hacking</span>
                 </div>
-                <div className='day_one row'>
-                	<div className='text_container column'>
-                	    <p className='date'>Jan 13th</p>
-                	    <p className='day_title'>Day 1</p>
-                	    <p>@ LAB Miami</p>
-                	    <p className='blue_text'>7:30PM - 9:00PM</p>
-                	    <p>Keynote Speaker - <a target='_blank' href='https://twitter.com/ToneVays'>Tone Vays</a></p>
-                	</div>
+                <div className='row'>
+                    <div className='day_one column'>
+                        <div className='text_container column'>
+                            <p className='date'>Jan 12th</p>
+                            <p className='day_title'>Workshop</p>
+                            <p>@ LAB Miami</p>
+                            <p className='blue_text'>7:00PM</p>
+                            <p>"Socratic Meetup" with <a target='_blank' href='https://www.meetup.com/Miami-International-Bitcoin-Group/events/236711565/'>BitDevs J and Max</a></p>
+                        </div>
+                    </div>
+                    <div className='day_one column'>
+                        <div className='text_container column'>
+                            <p className='date'>Jan 13th</p>
+                            <p className='day_title'>Day 1</p>
+                            <p>@ LAB Miami</p>
+                            <p className='blue_text'>7:30PM - 9:00PM</p>
+                            <p>Keynote Speaker - <a target='_blank' href='https://twitter.com/ToneVays'>Tone Vays</a></p>
+                        </div>
+                    </div>
                 </div>
+                
                 <div className='row'>
                 	<div className='day_two column'>
                 		<div className='text_container column'>
